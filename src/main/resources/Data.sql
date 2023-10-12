33c4f2a8692765a8046d1af08e62ec9730dd2e6d
nicola-inchingolo <nico.inchingolo03@gmail.com>
2023-10-12 16:25:36 +0200

    fine implementazione jwt

---
+++ src/main/resources/Data.sql
@@ -0,0 +1,6 @@
+insert into custom_user(nome,cognome,email,password,data_nascita,codice_fiscale, ruolo, attivo)
+values
+('Nicola','Inchingolo','nico.inchingolo03@gmail.com','Password!1','2003-08-05','NCHNCL03M05A662Z',3,true),
+('Matteo','Convertino','matteo.convertino@gmail.com','Password!2','2003-09-23','CNVMTT03M23S661Z',2,true),
+('Daniele','Cozzi','daniele.cozzi@gmail.com','Password!3','2003-03-18','CZZDNL03M18S663Z',1,true),
+('Edoardo','Loconte','edoardo.loconte@gmail.com','Password!4','2003-04-17','EDRLCN03M17S664Z',0,true);
