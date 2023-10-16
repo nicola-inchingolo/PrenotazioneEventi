a161ae8c4af7613df37c45c57d5b19e3308ee608
nicola-inchingolo <nico.inchingolo03@gmail.com>
2023-10-16 16:48:37 +0200

    inizio implementazione testing

---
+++ src/test/java/org/elis/softwareprenotazioneeventi/TestRepository.java
@@ -0,0 +1,36 @@
+package org.elis.softwareprenotazioneeventi;
+
+import org.elis.softwareprenotazioneeventi.model.Role;
+import org.elis.softwareprenotazioneeventi.model.User;
+import org.elis.softwareprenotazioneeventi.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class TestRepository {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testDimensioneElencoDati()
+    {
+        List<User> users = userRepository.findAllByRuolo(Role.SUPERADMIN);
+        assertEquals(1, users.size());
+    }
+
+    @Test
+    public void testLogin(){
+        assertThat(userRepository.findUserByEmailAndPassword("nico.inchingolo03@gmail.com", "Password!1"))
+                .get().extracting(User::getNome)
+                .isEqualTo("Nicola");
+    }
+
+
+}
